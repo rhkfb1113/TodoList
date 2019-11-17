00392b4440abf9c304f92fe61a76919fbe300835
rhkfb1113 <45762602+rhkfb1113@users.noreply.github.com>
2019-11-17 17:06:59 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ function addList() {
   const toDoForm = document.querySelector(".toDoForm");
   const toDoInput = toDoForm.querySelector("input").value;
   const node = document.createElement("li");
+  const removeButton = document.createElement("button");
   const textnode = document.createTextNode(toDoInput);
+
   node.appendChild(textnode);
+  node.appendChild(removeButton).innerHTML = "remove";
   node.onclick = function() {
     if (this.style.textDecoration == "line-through") {
       this.style.textDecoration = "none";
@@ -11,12 +14,19 @@ function addList() {
       this.style.textDecoration = "line-through";
     }
   };
+  removeButton.onclick = function() {
+    this.parentElement.remove(this);
+  };
   document.getElementById("myList").appendChild(node);
   document.getElementById("inputForm").value = null;
 }
 
 function enterkey() {
-  if(window.event.keyCode == 13) {
+  if (window.event.keyCode == 13) {
     addList();
   }
+}
+
+function removeAll() {
+  document.getElementById("myList").innerHTML = "";
 }
