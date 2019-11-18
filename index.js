9e980d08190f6e35d15bd72f4d5c996198999466
rhkfb1113 <45762602+rhkfb1113@users.noreply.github.com>
2019-11-18 16:56:49 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -4,29 +4,35 @@ function addList() {
   const node = document.createElement("li");
   const removeButton = document.createElement("button");
   const textnode = document.createTextNode(toDoInput);
-
+  removeButton.className = "btn btn-warning";
+  removeButton.style.cssText = "font-size: 10px; font-weight: 900; margin-left: 30px; margin-top: -1px; padding: 5px";
+  node.style.cssText = "font-size: 30x; margin-top: 3px; text-align: center; cursor: pointer;";
   node.appendChild(textnode);
-  node.appendChild(removeButton).innerHTML = "remove";
-  node.onclick = function() {
-    if (this.style.textDecoration == "line-through") {
-      this.style.textDecoration = "none";
-    } else {
-      this.style.textDecoration = "line-through";
-    }
-  };
-  removeButton.onclick = function() {
-    this.parentElement.remove(this);
-  };
+  node.appendChild(removeButton).innerHTML = "Del";
+  node.onclick = doneList;
+  removeButton.onclick = removeList;
   document.getElementById("myList").appendChild(node);
   document.getElementById("inputForm").value = null;
+}
+
+function removeList() {
+  this.parentElement.remove(this);
+}
+
+function doneList() {
+  if (this.style.textDecoration == "line-through") {
+    this.style.textDecoration = "none";
+  } else {
+    this.style.textDecoration = "line-through";
+  }
+}
+
+function removeAll() {
+  document.getElementById("myList").innerHTML = "";
 }
 
 function enterkey() {
   if (window.event.keyCode == 13) {
     addList();
   }
-}
-
-function removeAll() {
-  document.getElementById("myList").innerHTML = "";
 }
